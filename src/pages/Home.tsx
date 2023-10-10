8e6360ca27828f5f8775a3ece113d9f406456e63
bananabread08 <rmkfermin@gmail.com>
2023-10-10 11:09:00 +0800

    add animation in tw config and update Shop page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,45 @@
 import air75hero from '../assets/air75-v2-hero.webp'
 import field65hero from '../assets/field75-hero.webp'
+
+type HeroData = { imgUrl: string; title: string; desc: string }
+
+const heroData: HeroData[] = [
+  {
+    imgUrl: air75hero,
+    title: 'Air 75',
+    desc: 'The fastest and slimmest QMK/VIA keyboard on the planet',
+  },
+  {
+    imgUrl: field65hero,
+    title: 'Field 65',
+    desc: 'Everyday is field day',
+  },
+]
+
+const HeroImage = ({ data }: { data: HeroData }) => {
+  return (
+    <div className="relative overflow-hidden">
+      <img
+        src={data.imgUrl}
+        alt="air75-hero"
+        className="w-full h-full min-h-[375px] object-cover brightness-[85%] animate-fade-in"
+      ></img>
+      <div className="px-4 w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-primary-foreground backdrop-blur-sm ">
+        <h1 className="transition-all text-6xl sm:text-8xl md:text-9xl font-bold">
+          {data.title}
+        </h1>
+        <p className="transition-all text-xl md:text-2xl">{data.desc}</p>
+      </div>
+    </div>
+  )
+}
+
 export const Home = () => {
   return (
     <main>
-      <div className="relative">
-        <img
-          src={air75hero}
-          alt="air75-hero"
-          className="w-full h-full min-h-[375px] object-cover brightness-[85%]"
-        ></img>
-        <div className="px-4 w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-primary-foreground backdrop-blur-sm ">
-          <h1 className="transition-all text-6xl sm:text-8xl md:text-9xl font-bold">
-            Air 75
-          </h1>
-          <p className="transition-all text-xl md:text-2xl">
-            The fastest and slimmest QMK/VIA keyboard on the planet
-          </p>
-        </div>
-      </div>
-      <div className="relative">
-        <img
-          src={field65hero}
-          alt="field65-hero"
-          className="w-full h-full min-h-[375px] object-cover brightness-[85%] "
-        ></img>
-        <div className="px-4 w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-primary-foreground backdrop-blur-sm ">
-          <h1 className="transition-all text-6xl sm:text-8xl md:text-9xl font-bold">
-            Field 75
-          </h1>
-          <p className="transition-all text-xl md:text-2xl">
-            Everyday is field day
-          </p>
-        </div>
-      </div>
+      {heroData.map((data) => (
+        <HeroImage data={data} />
+      ))}
     </main>
   )
 }
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,3 +1,173 @@
+import air75v2Black from '../assets/air75-v2-black.webp'
+import air75v2White from '../assets/air75-v2-white.webp'
+import field75Electro from '../assets/field75-electro.webp'
+import field75Gravity from '../assets/field75-gravity.webp'
+import field75Noether from '../assets/field75-noether.webp'
+import halo65Black from '../assets/halo65-black.webp'
+import halo65White from '../assets/halo65-white.webp'
+import air60 from '../assets/air60.webp'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+type HyPhyProduct = {
+  name: string
+  price: number
+  status?: 'new' | 'discontinued'
+}
+
+type KeyboardVariant = {
+  color: string
+  img: string
+}
+
+const profile = ['Low', 'Normal'] as const
+const series = ['Air', 'Halo', 'Field'] as const
+const size = ['60', '65', '75'] as const
+
+type KBProfile = (typeof profile)[number]
+
+type KBSeries = (typeof series)[number]
+
+type KBSize = (typeof size)[number]
+
+type KeyboardTags = Array<KBProfile | KBSeries | KBSize>
+
+interface ProductKeyboard extends HyPhyProduct {
+  type: 'keyboard'
+  variant: KeyboardVariant[]
+  tags: KeyboardTags
+}
+
+const keybs: ProductKeyboard[] = [
+  {
+    name: 'Air75 V2 Wireless Custom Mechanical Keyboard',
+    type: 'keyboard',
+    price: 119.95,
+    status: 'new',
+    tags: ['75', 'Air', 'Low'],
+    variant: [
+      {
+        color: 'black',
+        img: air75v2Black,
+      },
+      {
+        color: 'white',
+        img: air75v2White,
+      },
+    ],
+  },
+  {
+    name: 'Field75 Wireless Mechanical Gaming Keyboard',
+    type: 'keyboard',
+    price: 159.95,
+    tags: ['75', 'Field', 'Normal'],
+    variant: [
+      {
+        color: 'electro',
+        img: field75Electro,
+      },
+      {
+        color: 'gravity',
+        img: field75Gravity,
+      },
+      {
+        color: 'noether',
+        img: field75Noether,
+      },
+    ],
+  },
+  {
+    name: 'Halo65 Wireless Mechanical Keyboard',
+    type: 'keyboard',
+    price: 119.95,
+    tags: ['65', 'Halo', 'Normal'],
+    variant: [
+      {
+        color: 'black',
+        img: halo65Black,
+      },
+      {
+        color: 'white',
+        img: halo65White,
+      },
+    ],
+  },
+  {
+    name: 'Air60 Wireless Mechanical Keyboard',
+    type: 'keyboard',
+    price: 99.95,
+    tags: ['60', 'Air', 'Low'],
+    variant: [
+      {
+        color: 'base',
+        img: air60,
+      },
+    ],
+  },
+]
+
+const ProductPreview = ({ keyb }: { keyb: ProductKeyboard }) => {
+  return (
+    <Card>
+      <CardContent className="p-0">
+        <div className="relative">
+          <img
+            src={keyb.variant[0].img}
+            alt={keyb.name}
+            className="w-full h-auto dark:brightness-75"
+          ></img>
+          {keyb.status === 'new' ? (
+            <div className="absolute top-4 left-4 bg-emerald-400 dark:bg-green-600 text-white px-2 rounded-full uppercase font-bold">
+              New!
+            </div>
+          ) : null}
+        </div>
+      </CardContent>
+      <CardHeader>
+        <CardTitle className="text-base">{keyb.name}</CardTitle>
+        <CardDescription>Price: ${keyb.price}</CardDescription>
+      </CardHeader>
+    </Card>
+  )
+}
+
 export const Shop = () => {
-  return <div>This is the Shop Page</div>
+  return (
+    <section>
+      <div className="flex flex-col sm:flex-row">
+        <aside className="w-full sm:w-[200px] flex flex-col items-center">
+          <h1 className="font-bold">Sort</h1>
+          <div>
+            <h1>Keyboard Profile</h1>
+            {profile.map((p) => (
+              <div>{p}</div>
+            ))}
+          </div>
+          <div>
+            <h1>Series</h1>
+            {series.map((s) => (
+              <div>{s}</div>
+            ))}
+          </div>
+          <div>
+            <h1>Size</h1>
+            {size.map((sz) => (
+              <div>{sz}</div>
+            ))}
+          </div>
+        </aside>
+        <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+          {keybs.map((keyb) => {
+            return <ProductPreview key={keyb.name} keyb={keyb} />
+          })}
+        </div>
+      </div>
+      <hr />
+    </section>
+  )
 }
