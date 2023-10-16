150032d34bb32c4217c1a8ce1ca342fefb80fc5f
bananabread08 <rmkfermin@gmail.com>
2023-10-16 10:10:19 +0800

    revert to original SheetPortal Primitive

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -11,12 +11,14 @@ const SheetTrigger = SheetPrimitive.Trigger
 
 const SheetClose = SheetPrimitive.Close
 
-const SheetPortal = ({
-  className,
-  ...props
-}: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
-)
+const SheetPortal = SheetPrimitive.Portal
+
+// const SheetPortal = ({
+//   className,
+//   ...props
+// }: SheetPrimitive.DialogPortalProps) => (
+//   <SheetPrimitive.Portal className={cn(className)} {...props} />
+//)
 SheetPortal.displayName = SheetPrimitive.Portal.displayName
 
 const SheetOverlay = React.forwardRef<
