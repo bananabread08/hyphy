64db1860fece7e264129e369d49f7be05ce2367a
bananabread08 <rmkfermin@gmail.com>
2023-10-23 15:57:13 +0800

    create routesConfig for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { RootLayout } from './components/RootLayout'
-import { NotFound } from './pages/NotFound'
-import { Home } from './pages/Home'
-import { Shop } from './pages/Shop'
 import { ThemeProvider } from './context/ThemeContext'
-import { loader as productLoader } from './components/Product/Product.loader'
-import { loader as shopLoader } from './components/Product/Shop.loader'
-import { Product } from './pages/Product'
 import { CartProvider } from './context/CartContext'
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/shop',
-        element: <Shop />,
-        loader: shopLoader,
-      },
-      {
-        path: '/shop/:id',
-        element: <Product />,
-        loader: productLoader,
-      },
-    ],
-  },
-])
+import { routesConfig } from './routesConfig'
+
+const router = createBrowserRouter(routesConfig)
 
 function App() {
   return (
